f885407dd99fdb3a04e870f0f87ec5de033253b6
beellz <belal20795@gmail.com>
2020-06-11 23:11:32 +0530

    {commit}

--- gitS.py
+++ gitS.py
@@ -1,10 +1,15 @@
 import os
 import sys
+import time
 
 os.system("git status")
 
 os.system("git add *")
+
+
+time.sleep(2)
 commit = sys.argv[1] 
+
 
 
 print (f" The commit u are adding is  {commit}")
