fafb68674fe5645b90a0d5c0237a63927c3b9f8f
beellz <belal20795@gmail.com>
2020-06-12 00:00:30 +0530

    [hello this is 3rd commit]

--- gitS.py
+++ gitS.py
@@ -8,13 +8,15 @@ os.system("git add *")
 
 
 #sleep(2)
-commit = sys.argv[1]
+commit = sys.argv[1:]
 
-NewC = str("commit")
+NewC = str(commit)
 
 
 print (f" The commit u are adding is  {NewC}")
 
+#print (f" The commit u are adding is  {commit}")
 
-os.system('git commit -m' +NewC)
+
+os.system("git commit -m " +NewC)
 
